d2ea4a28bc2d0a20888d1c7c4c19604cb6724994
igorfals <igorfeals@gmail.com>
2025-09-25 23:48:16 -0300

    feat: adjust model mappings and add timestamps to Order

--- prisma/migrations/20250713071303_init_user_and_order_schema/migration.sql
+++ prisma/migrations/20250713071303_init_user_and_order_schema/migration.sql
@@ -1,19 +1,21 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id_user" SERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id_user")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Order" (
-    "id_order" SERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "client_name" TEXT NOT NULL,
     "estimation_date" TIMESTAMP(3) NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "address" TEXT NOT NULL,
     "cellphone" TEXT NOT NULL,
     "city" TEXT NOT NULL,
@@ -21,11 +23,11 @@ CREATE TABLE "Order" (
     "items" JSONB NOT NULL,
     "cod_user" INTEGER NOT NULL,
 
-    CONSTRAINT "Order_pkey" PRIMARY KEY ("id_order")
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_cod_user_fkey" FOREIGN KEY ("cod_user") REFERENCES "User"("id_user") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_cod_user_fkey" FOREIGN KEY ("cod_user") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
